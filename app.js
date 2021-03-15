49be750db55f0cfc0c05f39e7bf0519d0b5f1228
Nicole George <hello@nicolegeorge.me>
2021-03-15 13:27:17 +1030

    fetching random data - fetching name of beer successfully

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://api.punkapi.com/v2/beers")
+  fetch("https://api.punkapi.com/v2/beers/random")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
+      const name = data[0].name;
+      console.log(name);
+      
     });
 });
